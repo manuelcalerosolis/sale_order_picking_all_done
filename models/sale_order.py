e85fe99ae2b860ffbe24511e50ee428d180325fd
manuelcalerosolis <mcalero@gestool.es>
2019-11-11 19:16:01 +0100

    confirm and invoiced

--- models/sale_order.py
+++ models/sale_order.py
@@ -10,7 +10,7 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     @api.multi
-    def action_sale_order_all_done(self):
+    def action_sale_order_confirm_and_delivery(self):
         self.action_confirm()
 
         for picking in self.picking_ids:
@@ -19,4 +19,25 @@ class SaleOrder(models.Model):
                 line.quantity_done = line.product_uom_qty
 
             picking.button_validate()
+
+    @api.multi
+    def action_sale_order_confirm_and_invoiced(self):
+        self.action_sale_order_confirm_and_delivery()
+
+        self.action_invoice_create()
+
+        for invoice in self.invoice_ids:
+            invoice.action_invoice_open()
+
+        # payment = self.env['sale.advance.payment.inv'].create({'advance_payment_method': 'delivered'})
+        #
+        # sale_context = {'active_id': self.id,
+        #                 'active_ids': self.ids,
+        #                 'active_model': 'sale.order',
+        #                 'open_invoices': True}
+        #
+        #
+        # res = payment.with_context(sale_context).create_invoices()
+        #
+        # return self.env['account.invoice'].browse(res['res_id'])
 
